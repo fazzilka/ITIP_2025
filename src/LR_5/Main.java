299ddc387d3f98aeb49727a0a9386402aeee5f1d
fazzilka <fazzil349@gmail.com>
2025-11-28 16:39:06 +0300

    labs

--- src/LR_5/Main.java
+++ src/LR_5/Main.java
@@ -14,9 +14,9 @@ public class Main{
             System.out.println(CorrectPassword("a111111111"));
             System.out.println(CorrectPassword("1111111111"));
             System.out.println(UppercaseAfterLowercase("aaaWbbzSbjjjjjjMMkkkkkkk"));
-            System.out.println(FindWord("Hello dear friend this morning is for Backend and DB", "d"));
             System.out.println(CorrectIP("1.243.54.0"));
             System.out.println(CorrectIP("1.263.54.0"));
+            System.out.println(FindWord("Hello dear friend this morning is for Backend and DB", "d"));
         }
         catch(PatternSyntaxException e){
             System.out.println("Incorrect template syntax"+ e);
