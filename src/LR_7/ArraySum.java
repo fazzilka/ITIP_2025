9a6c879035d4158f434f60fc4d5467a6e4bc8594
fazzilka <fazzil349@gmail.com>
2025-12-15 19:48:13 +0300

    labs

--- src/LR_7/ArraySum.java
+++ src/LR_7/ArraySum.java
@@ -26,7 +26,7 @@ public class ArraySum{
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int mid = array.length / 2;
 
